3f8a03072f05a9255b346bf020dd3a75e7ffa874
K4zoku <kazoku0356@gmail.com>
2022-08-01 02:35:04 +0700

    ok

--- src/main/java/io/gitlab/zeromatter/yomikaze/controller/WakeController.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/controller/WakeController.java
@@ -1,13 +1,16 @@
 package io.gitlab.zeromatter.yomikaze.controller;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Log
 @Controller
 public class WakeController {
     @RequestMapping("/wake")
     public @ResponseBody String wake() {
+        log.info("ok");
         return "ok"; // one punch man
     }
 }
