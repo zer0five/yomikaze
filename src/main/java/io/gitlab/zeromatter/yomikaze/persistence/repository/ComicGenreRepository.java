366d20e88806c05cc0279638e831be8aea2a2fa4
quocviet <thaibinhquocviet2002@gmail.com>
2022-10-05 15:37:45 +0700

    update Comic genre relation

--- src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/ComicGenreRepository.java
+++
@@ -1,8 +0,0 @@
-package io.gitlab.zeromatter.yomikaze.persistence.repository;
-
-import io.gitlab.zeromatter.yomikaze.persistence.entity.Comic;
-import io.gitlab.zeromatter.yomikaze.persistence.entity.ComicGenre;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ComicGenreRepository extends CrudRepository<ComicGenre, Comic> {
-}
--- src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/ComicRepository.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/ComicRepository.java
@@ -1,7 +1,5 @@
 package io.gitlab.zeromatter.yomikaze.persistence.repository;
 
-import io.gitlab.zeromatter.yomikaze.persistence.entity.Comic;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ComicRepository extends CrudRepository<Comic, Integer> {
