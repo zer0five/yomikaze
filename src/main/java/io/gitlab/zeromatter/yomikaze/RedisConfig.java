7f1e5a26ed4b624d2350b1f49f60d8433ce1bf66
K4zoku <kazoku0356@gmail.com>
2022-10-15 02:20:57 +0700

    ...

--- src/main/java/io/gitlab/zeromatter/yomikaze/RedisConfig.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/RedisConfig.java
@@ -1,16 +1,18 @@
 package io.gitlab.zeromatter.yomikaze;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 import java.time.Duration;
@@ -40,13 +42,23 @@ public class RedisConfig {
 
     @Profile("heroku")
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        String redisUrl = System.getenv("REDIS_URL");
-        if (redisUrl == null) {
+    public RedisConnectionFactory redisConnectionFactory(Environment environment) {
+        String redisUrl = environment.getProperty("REDIS_URL", "");
+        if (redisUrl.isEmpty()) {
             log.info("REDIS_URL is not set, not using redis");
             return null;
         }
         log.info("REDIS_URL is set to " + redisUrl);
-        return new JedisConnectionFactory(new RedisStandaloneConfiguration(redisUrl));
+
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisUrl));
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return clientConfigurationBuilder -> {
+            if (clientConfigurationBuilder.build().isUseSsl()) {
+                clientConfigurationBuilder.useSsl().disablePeerVerification();
+            }
+        };
     }
 }
