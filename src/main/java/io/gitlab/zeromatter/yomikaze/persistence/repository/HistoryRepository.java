ed817ce633119a0607a7d1751a73260fe26b7199
K4zoku <kazoku0356@gmail.com>
2022-10-15 00:45:44 +0700

    added required method for HistoryRepository

--- src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/HistoryRepository.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/HistoryRepository.java
@@ -1,8 +1,14 @@
 package io.gitlab.zeromatter.yomikaze.persistence.repository;
 
+import io.gitlab.zeromatter.yomikaze.persistence.entity.Account;
 import io.gitlab.zeromatter.yomikaze.persistence.entity.History;
 import io.gitlab.zeromatter.yomikaze.snowflake.Snowflake;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface HistoryRepository extends CrudRepository<History, Snowflake> {
+    Page<History> findAllByAccountId(Snowflake accountId, Pageable pageable);
+
+    Page<History> findAllByAccount(Account account, Pageable pageable);
 }
