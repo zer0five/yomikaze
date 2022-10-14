50790ea02596fc6378ad396bf2b70cb463820d5d
K4zoku <kazoku0356@gmail.com>
2022-10-15 02:35:18 +0700

    Added ConfigureRedisAction Bean

--- src/main/java/io/gitlab/zeromatter/yomikaze/RedisConfig.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Slf4j
@@ -22,6 +23,11 @@ public class RedisConfig {
                 clientConfigurationBuilder.useSsl().disablePeerVerification();
             }
         };
+    }
+
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
     }
 
 }
