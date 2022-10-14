8932ea8b9f3ad3bda5b8a39a31966973800a91a8
K4zoku <kazoku0356@gmail.com>
2022-10-15 00:49:34 +0700

    Removed unnecessary code

--- src/main/java/io/gitlab/zeromatter/yomikaze/config/JpaPersistenceConfig.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/config/JpaPersistenceConfig.java
@@ -1,12 +1,6 @@
 package io.gitlab.zeromatter.yomikaze.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import io.gitlab.zeromatter.yomikaze.persistence.entity.Comic;
-import io.gitlab.zeromatter.yomikaze.persistence.entity.Genre;
-import io.gitlab.zeromatter.yomikaze.persistence.repository.ComicRepository;
-import io.gitlab.zeromatter.yomikaze.persistence.repository.GenreRepository;
-import io.gitlab.zeromatter.yomikaze.persistence.repository.PermissionRepository;
-import io.gitlab.zeromatter.yomikaze.persistence.repository.RoleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.cache.annotation.EnableCaching;
@@ -19,16 +13,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import javax.sql.DataSource;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Configuration
 @EnableCaching
 @EnableJpaRepositories(basePackages = "io.gitlab.zeromatter.yomikaze.persistence.repository")
 @Slf4j
 public class JpaPersistenceConfig {
-    boolean isInitialized = false;
-
     @Bean
     @Primary
     @Profile("heroku")
@@ -57,55 +48,5 @@ public class JpaPersistenceConfig {
                 .username(username)
                 .password(password)
                 .build();
-    }
-
-    @Bean
-    public boolean initData(GenreRepository genreRepository, ComicRepository comicRepository, RoleRepository roleRepository, PermissionRepository permissionRepository) {
-        if (isInitialized) {
-            return true;
-        }
-        String[] genresStr = new String[]{
-                "Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Psychological", "Romance", "Sci-Fi", "Slice of Life", "Supernatural", "Thriller"
-        };
-        Map<String, Genre> genres = new HashMap<>();
-        for (String genre : genresStr) {
-            Optional<Genre> og = genreRepository.findByName(genre);
-            Genre g;
-            if (og.isPresent()) {
-                g = og.get();
-            } else {
-                g = new Genre();
-                g.setName(genre);
-                g = genreRepository.save(g);
-            }
-            genres.put(genre, g);
-        }
-
-        String[] titles = new String[]{
-                "Comic 001", "Comic 2"
-        };
-        String[][] comicGenres = new String[][]{
-                {"Action", "Adventure"},
-                {"Action", "Adventure", "Drama"}
-        };
-        for (int i = 0; i < titles.length; i++) {
-            Comic comic = new Comic();
-            comic.setTitle(titles[i]);
-
-            for (String genre : comicGenres[i]) {
-                comic.getGenres().add(genres.get(genre));
-            }
-            comicRepository.save(comic);
-        }
-        log.info("Data initialized");
-        Set<Genre> whitelist = new HashSet<>();
-        whitelist.add(genres.get("Action"));
-        whitelist.add(genres.get("Adventure"));
-        Set<Genre> blacklist = new HashSet<>(whitelist);
-        blacklist.add(genres.get("Drama"));
-        Set<Comic> filtered = comicRepository.findByGenresContainsAllAndNone(whitelist, blacklist);
-        log.info("Filtered: " + filtered.stream().map(Comic::getTitle).collect(Collectors.joining(", ")));
-        isInitialized = true;
-        return true;
     }
 }
