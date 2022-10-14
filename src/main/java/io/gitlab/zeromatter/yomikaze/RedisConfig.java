15c27f949d2f323dd220b503db6f18a2690b288c
K4zoku <kazoku0356@gmail.com>
2022-10-15 02:25:59 +0700

    Fixed dependencies (maybe not)

--- src/main/java/io/gitlab/zeromatter/yomikaze/RedisConfig.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/RedisConfig.java
@@ -2,22 +2,11 @@ package io.gitlab.zeromatter.yomikaze;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Configuration
@@ -25,33 +14,6 @@ import java.util.Map;
 @Profile("heroku")
 @EnableRedisRepositories
 public class RedisConfig {
-
-    @Profile("heroku")
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        Map<String, RedisCacheConfiguration> cacheNamesConfigurationMap = new HashMap<>();
-
-        cacheNamesConfigurationMap.put("TimeZone", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(30)));
-        return RedisCacheManager
-                .RedisCacheManagerBuilder
-                .fromConnectionFactory(connectionFactory)
-                .withInitialCacheConfigurations(cacheNamesConfigurationMap)
-                .build();
-
-    }
-
-    @Profile("heroku")
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(Environment environment) {
-        String redisUrl = environment.getProperty("REDIS_URL", "");
-        if (redisUrl.isEmpty()) {
-            log.info("REDIS_URL is not set, not using redis");
-            return null;
-        }
-        log.info("REDIS_URL is set to " + redisUrl);
-
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisUrl));
-    }
 
     @Bean
     public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
