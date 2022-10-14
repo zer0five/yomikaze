b74956606a492d8eea3f79c746be322f92cbc89c
K4zoku <kazoku0356@gmail.com>
2022-10-15 00:48:30 +0700

    add required method for LibraryRepository

--- src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/LibraryRepository.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/persistence/repository/LibraryRepository.java
@@ -3,12 +3,14 @@ package io.gitlab.zeromatter.yomikaze.persistence.repository;
 import io.gitlab.zeromatter.yomikaze.persistence.entity.Account;
 import io.gitlab.zeromatter.yomikaze.persistence.entity.Library;
 import io.gitlab.zeromatter.yomikaze.snowflake.Snowflake;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-
 @Repository
 public interface LibraryRepository extends CrudRepository<Library, Snowflake> {
-    Collection<Library> findAllByAccount(Account account);
+    Page<Library> findAllByAccount(Account account, Pageable pageable);
+
+    Page<Library> findAllByAccountId(Snowflake accountId, Pageable pageable);
 }
