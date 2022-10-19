7f0aea3adc9c54117833e7940136b6cece10cd5e
Radkaim <hungct04@gmail.com>
2022-10-19 17:47:45 +0700

    Added validation for sign up

--- src/main/java/io/gitlab/zeromatter/yomikaze/web/dto/RegistrationData.java
+++ src/main/java/io/gitlab/zeromatter/yomikaze/web/dto/RegistrationData.java
@@ -1,11 +1,14 @@
 package io.gitlab.zeromatter.yomikaze.web.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
+@NoArgsConstructor
 public class RegistrationData {
 
     @Pattern(regexp = "^\\w{3,16}$", message = "registration.username.invalid")
@@ -14,6 +17,9 @@ public class RegistrationData {
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "registration.password.invalid")
     private String password;
 
-    @Email
+    @Email(message = "registration.email.invalid")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$", message = "registration.password.invalid")
+    private String passwordConfirmation;
 }
