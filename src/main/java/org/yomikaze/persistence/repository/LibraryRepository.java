47af2249c2a51eb58f5e75f028b4e8a72a30c96f
Radkaim <hungct04@gmail.com>
2022-10-26 18:11:06 +0700

    Added add and remove in LibraryRepository

--- src/main/java/org/yomikaze/persistence/repository/LibraryRepository.java
+++ src/main/java/org/yomikaze/persistence/repository/LibraryRepository.java
@@ -5,12 +5,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.yomikaze.persistence.entity.Account;
+import org.yomikaze.persistence.entity.Comic;
 import org.yomikaze.persistence.entity.Library;
 import org.yomikaze.snowflake.Snowflake;
+
+import java.util.Optional;
 
 @Repository
 public interface LibraryRepository extends CrudRepository<Library, Snowflake> {
     Page<Library> findAllByAccount(Account account, Pageable pageable);
 
     Page<Library> findAllByAccountId(Snowflake accountId, Pageable pageable);
+
+    Optional<Library> findByAccountAndComic(Account account, Comic comic);
+
+    Optional<Library> findByAccountAndComic_Id(Account account, Snowflake comic_id);
 }
--- src/main/java/org/yomikaze/web/controller/LibraryController.java
+++ src/main/java/org/yomikaze/web/controller/LibraryController.java
@@ -4,15 +4,23 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yomikaze.persistence.entity.Account;
+import org.yomikaze.persistence.entity.Comic;
+import org.yomikaze.persistence.entity.Library;
+import org.yomikaze.persistence.repository.ComicRepository;
 import org.yomikaze.persistence.repository.LibraryRepository;
+import org.yomikaze.snowflake.Snowflake;
 
+import java.net.URI;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +30,7 @@ import java.util.Optional;
 public class LibraryController {
 
     private final LibraryRepository libraryRepository;
+    private final ComicRepository comicRepository;
 
     @GetMapping({"", "/"})
     public String library(Optional<Integer> page, Optional<Integer> size, Model model, Authentication authentication) {
@@ -32,4 +41,34 @@ public class LibraryController {
         model.addAttribute("comics", libraryRepository.findAllByAccount(account, pageable));
         return "views/library/index";
     }
+
+    @GetMapping("/add/{id}")
+    public ResponseEntity<Object> add(Authentication authentication, @PathVariable("id") Snowflake id) {
+        Account account = (Account) authentication.getPrincipal();
+        Optional<Comic> comic = comicRepository.findById(id);
+        if (!comic.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        Optional<Library> library = libraryRepository.findByAccountAndComic(account, comic.get());
+        if (library.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        Library newLibrary = new Library();
+        newLibrary.setAccount(account);
+        newLibrary.setComic(comic.get());
+        libraryRepository.save(newLibrary);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/remove/{id}")
+    public ResponseEntity<Object> remove(Authentication authentication, @PathVariable("id") Snowflake id){
+        Account account = (Account) authentication.getPrincipal();
+        Optional<Library> library = libraryRepository.findByAccountAndComic_Id(account, id);
+        if(!library.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        libraryRepository.delete(library.get());
+        return ResponseEntity.ok().build();
+    }
+
 }
