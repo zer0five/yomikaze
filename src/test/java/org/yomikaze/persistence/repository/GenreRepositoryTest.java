37bfbe63c392ad16a48157f87d9ff62618941329
Radkaim <hungct04@gmail.com>
2022-10-28 17:17:47 +0700

    Added test case for GenreRepositoryTest

---
+++ src/test/java/org/yomikaze/persistence/repository/GenreRepositoryTest.java
@@ -0,0 +1,38 @@
+package org.yomikaze.persistence.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.yomikaze.persistence.entity.Genre;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class GenreRepositoryTest {
+
+    @Autowired
+    ComicRepository comicRepository;
+    @Autowired
+    GenreRepository genreRepository;
+
+   @Test
+    void testfindByName() {
+       Iterable<Genre> genres = genreRepository.saveAll(Arrays.asList(
+           new Genre("Action"),
+           new Genre("Adventure"),
+           new Genre("Comedy"),
+           new Genre("Drama"),
+           new Genre("Fantasy")
+       ));
+
+       Optional<Genre> genre = genreRepository.findByName("Action");
+
+       assertEquals("Action",genre.get().getName());
+
+   }
+}
