0b8dfd7d7eacae0483a4b61102d1d1dd0f827d46
Radkaim <hungct04@gmail.com>
2022-11-06 12:11:14 +0700

    add status and toggle, add index

--- src/main/java/org/yomikaze/web/controller/LibraryController.java
+++ src/main/java/org/yomikaze/web/controller/LibraryController.java
@@ -2,6 +2,7 @@ package org.yomikaze.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.yomikaze.persistence.entity.Account;
+import org.yomikaze.persistence.entity.Comic;
 import org.yomikaze.persistence.entity.Library;
 import org.yomikaze.service.LibraryService;
 import org.yomikaze.snowflake.Snowflake;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,7 +37,9 @@ public class LibraryController {
     public String library(Pageable pageable, Model model, Authentication authentication) {
         Account account = (Account) authentication.getPrincipal();
         Page<Library> libraries = libraryService.getLibrary(account, pageable);
-        model.addAttribute("libraries", libraries);
+        List<Comic> comicList = libraries.getContent().stream().map(Library::getComic).collect(Collectors.toList());
+        Page<Comic> comics = new PageImpl<>(comicList, pageable, libraries.getTotalElements());
+        model.addAttribute("comics", comics);
         return "views/library/index";
     }
 
@@ -51,14 +57,28 @@ public class LibraryController {
         return ResponseEntity.ok().build();
     }
 
+    @GetMapping("/toggle/{id}")
+    public ResponseEntity<Object> toggle(Authentication authentication, @PathVariable("id") Snowflake id) {
+        Account account = (Account) authentication.getPrincipal();
+        libraryService.toggleComic(account, id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/status/{id}")
+    public ResponseEntity<Object> status(Authentication authentication, @PathVariable("id") Snowflake id) {
+        Account account = (Account) authentication.getPrincipal();
+        boolean inLibrary = libraryService.isInLibrary(account, id);
+        return ResponseEntity.ok(inLibrary);
+    }
+
     @ExceptionHandler({EntityNotFoundException.class})
     public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException e) {
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler({EntityExistsException.class})
     public ResponseEntity<Object> handleEntityExistsException(EntityExistsException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
     }
 
 }
